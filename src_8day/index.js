ec2011017e1d224a2b08a7c44f09d3ead746197b
Dayglow0926 <dayglow0926@gmail.com>
2022-11-12 02:03:06 +0900

    fix: 챌린지 기본 내용

--- src_8day/index.js
+++ src_8day/index.js
@@ -1,12 +1,16 @@
-import "./models/Movie";
+import "./db.js";
 import express from "express";
 import path from "path";
 import bodyParser from "body-parser";
+import movieRouter from "./movieRouter.js";
+import { localsMiddleware } from "./middlewares.js";
 
 const app = express();
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(localsMiddleware);
+app.use("/", movieRouter);
 
 // Codesanbox does not need PORT :)
-app.listen(() => console.log(`✅  Server Ready!`));
+app.listen(4003, () => console.log(`✅  Server Ready!`));
